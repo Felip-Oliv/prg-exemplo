6de9c6ad4128f99df9222f8fcfafd76cb20b21c6
Luiz Felipe A. Oliveira <67592458+Felip-Oliv@users.noreply.github.com>
2022-06-26 16:45:21 -0300

    add seletor de itens
    home component

--- src/app/home/seletor/seletor.component.ts
+++ src/app/home/seletor/seletor.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seletor.component.css']
 })
 export class SeletorComponent implements OnInit {
-
+  quantidade = 1;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  adicionarItem(){
+    this.quantidade++;
+    console.log("quantidade: " + this.quantidade);
+  }
+
+  removerItem(){
+    if(this.quantidade > 1){
+      this.quantidade--;
+      console.log("quantidade: " + this.quantidade);
+    }else{
+      console.log("Não é possível remover");
+    }
+
+    }
+
 
 }
